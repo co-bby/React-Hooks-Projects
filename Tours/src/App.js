5e80aad367410163fb9703548029ce32c663d1fa
co-bby <nkumsagoefredrick@gmail.com>
2021-12-22 21:46:17 -0500

    fixed a bug

--- Tours/src/App.js
+++ Tours/src/App.js
@@ -1,11 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tour, setTour] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTour(tours);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tour={tour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- Tours/src/Tour.js
+++ Tours/src/Tour.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
-
-const Tour = () => {
-  return <h2>tour component</h2>;
+import React, { useState } from "react";
+const Tour = ({ id, image, info, price, name }) => {
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <foter>
+        <div className="tour-side">
+          <h4>{name}</h4>
+        </div>
+      </foter>
+    </article>
+  );
 };
 
 export default Tour;
