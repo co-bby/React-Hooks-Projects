3c1a09a85bb046240a040a0e5af65d9e990d47a9
co-bby <nkumsagoefredrick@gmail.com>
2022-01-07 20:22:59 -0500

    review setup

---
+++ reviews/src/Review.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+
+  const { name, job, image, text } = people[index];
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+    </article>
+  );
+};
+
+export default Review;
