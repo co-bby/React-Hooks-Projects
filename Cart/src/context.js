b96a1e1779c75c0d77cd4dd77fd5967f290342e9
co-bby <nkumsagoefredrick@gmail.com>
2022-01-24 09:39:59 -0500

    incre and decre btn

--- Cart/src/context.js
+++ Cart/src/context.js
@@ -19,10 +19,23 @@ const AppProvider = ({ children }) => {
   const clearCart = () => {
     dispatch({ type: "CLEAR_CART" });
   };
+  const remove = (id) => {
+    dispatch({ type: "REMOVE", payload: id });
+  };
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
   return (
     <AppContext.Provider
       value={{
         ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
       }}
     >
       {children}
